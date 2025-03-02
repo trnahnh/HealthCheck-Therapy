43d47f68eb05fdca35af91f5c8bcac84e96646c7
Mochi <70058325+CheVietBaoLong@users.noreply.github.com>
2025-03-01 21:13:58 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-document.getElementById('userForm').addEventListener('submit', function (e) {
+document.getElementById('userProfileForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
+    const height = document.getElementById('height').value;
+    const weight = document.getElementById('weight').value;
+    const skinTone = document.getElementById('skinTone').value;
+    const dateOfBirth = document.getElementById('dateOfBirth').value;
+    const gender = document.getElementById('gender').value;
 
-    const userData = `Name: ${name}\nEmail: ${email}\n`;
+
+
+    const userData = `Name: ${name}\nEmail: ${email}\nHeight: ${height}\nWeight: ${weight}\nSkin Tone: ${skinTone}\nDate Of Birth: ${dateOfBirth}\nGender: ${gender}`;
     const blob = new Blob([userData], { type: 'text/plain' });
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement('a');
